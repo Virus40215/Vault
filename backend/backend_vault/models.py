9ade98e0d8a41b3c869e94bc526542144bb47859
LDM158 <linodemarco95@gmail.com>
2025-02-13 16:26:33 +0100

    Added Relation Snippet/User

--- backend/backend_vault/models.py
+++ backend/backend_vault/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Snippet(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
     title = models.CharField(max_length=255, null=False) 
     language = models.CharField(max_length=50, null=False)
     description = models.TextField(blank=True, null=True) 
